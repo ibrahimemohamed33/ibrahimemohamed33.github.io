932904eff48fdf955f07b82149422d3754211694
Ibrahim E. Mohamed <ibrahimemohamed33@gmail.com>
2024-05-14 12:31:20 -0400

    fixing as i go

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Ibrahim Mohamed";
+export const SITE_TITLE = "Ibrahim's Personal Website";
 export const SITE_DESCRIPTION = "Personal Website for Ibrahim E. Mohamed";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
