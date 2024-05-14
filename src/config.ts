0b80e4a17f89b2201bc6656086fed24d052c16a2
Ibrahim E. Mohamed <ibrahimemohamed33@gmail.com>
2024-05-14 08:12:36 -0400

    fixing as i go

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@
 
 export const SITE_TITLE = "Ibrahim Mohamed";
 export const SITE_DESCRIPTION = "Personal Website for Ibrahim E. Mohamed";
-export const GENERATE_SLUG_FROM_TITLE = true;
+export const GENERATE_SLUG_FROM_TITLE = false;
 export const TRANSITION_API = true;
