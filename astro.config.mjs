2a4f8267ee8513cb9c3d07cdab8464bc6546727b
Ibrahim E. Mohamed <ibrahimemohamed33@gmail.com>
2024-05-14 20:53:02 -0400

    more fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ibrahimemohamed33.github.io/',
-  base: '',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
